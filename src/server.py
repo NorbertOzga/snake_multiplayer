1eea4833dd3b4f0ebb2cd84466760bfe91f28d53
Antoni-Pietryga <pietryga.antek@gmail.com>
2021-06-16 22:40:01 +0200

    zmiany

--- src/server.py
+++ src/server.py
@@ -118,7 +118,7 @@ class UDPServer:
             can_join = 1 if self.games[game_id]["players_num"] < 2 else 0
             game_name = self.games[game_id]["game_name"]
             game_info_list.append({"game_id": game_id, "can_join": can_join, "game_name": game_name})
-        print(self.games)
+        print(game_info_list)
         body = Body()
         header = Header(sender=0, message_type=MessageType.LIST_GAMES_SERVER)
         body.data["operation_success"] = b'\x20'
