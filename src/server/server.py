196f943dc6e4df24dff2b1f3d1b0d188fe7d0710
Antoni-Pietryga <pietryga.antek@gmail.com>
2021-06-16 20:41:40 +0200

    zmiany

--- src/server/server.py
+++ src/server/server.py
@@ -2,7 +2,7 @@ import socket
 from datetime import datetime
 import random
 import time
-from ..venom import *
+from src.venom.venom import *
 
 class UDPServer:
     games = {}  # list of current games
