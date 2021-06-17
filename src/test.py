14c3da830e3b8324923a87227503f4ace410c3c1
Antoni-Pietryga <pietryga.antek@gmail.com>
2021-06-17 11:41:15 +0200

    zmiany

--- src/test.py
+++ src/test.py
@@ -6,6 +6,14 @@ import datetime as dt
 from mbedtls import hashlib
 from mbedtls import pk
 from mbedtls import x509
+import socket
+from contextlib import suppress
+import multiprocessing as mp
+
+def block(callback, *args, **kwargs):
+     while True:
+         with suppress(tls.WantReadError, tls.WantWriteError):
+             return callback(*args, **kwargs)
 
 now = dt.datetime.utcnow()
 ca0_key = pk.RSA()
@@ -43,3 +51,25 @@ dtls_cli_ctx = tls.ClientContext(tls.DTLSConfiguration(
      trust_store=trust_store,
      validate_certificates=True,
 ))
+
+dtls_srv = dtls_srv_ctx.wrap_socket(
+    socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+)
+
+def dtls_server_main_loop(sock):
+     """A simple DTLS echo server."""
+     conn, addr = sock.accept()
+     conn.setcookieparam(addr[0].encode())
+     with suppress(tls.HelloVerifyRequest):
+        block(conn.do_handshake)
+     conn, addr = conn.accept()
+     conn.setcookieparam(addr[0].encode())
+     block(conn.do_handshake)
+     data = conn.recv(4096)
+     conn.send(data)
+
+port = 10000
+dtls_srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+dtls_srv.bind(("0.0.0.0", port))
+runner = mp.Process(target=dtls_server_main_loop, args=(dtls_srv, ))
+runner.start()
