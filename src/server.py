26398f1b5a7d1d85559a736d167f415ce0a15f1a
Antoni-Pietryga <pietryga.antek@gmail.com>
2021-06-17 02:17:51 +0200

    zmiany

--- src/server.py
+++ src/server.py
@@ -348,7 +348,7 @@ class UDPServer:
     def check_games(self):
         now = time.time()
         print(now, "check_games")
-        for i in range(100):
+        for i in range(1000000):
             a = i**4
         for game_id in self.queue.keys():
             recive_time, hosts = self.queue[game_id]
