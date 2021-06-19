a36ed3f886fee910d16094751a0bb8d7ccf3f34d
Magnetic-Fox <magnetic-fox@hotmail.com>
2021-06-19 20:15:00 +0200

    Clean client code

--- src/client.py
+++ src/client.py
@@ -2,7 +2,6 @@
 
 # Import section
 import threading
-
 import pygame
 from pygame.locals import *
 import sys
@@ -15,8 +14,10 @@ import os
 import ssl
 
 # Server and its address configuration
-SERVER_ADDRESS = "20.93.184.26"
-SERVER_PORT = 10000
+SERVER_ADDRESS = "20.93.184.26" # IPv4 address
+SERVER_PORT = 10000             # server port
+
+# Player name
 MYNAME = ""
 
 # Points
@@ -33,13 +34,13 @@ def drawPoints(player1_points, player2_points):
     P2_PT = font.render(str(player2_points).zfill(8), True, WHITE)
     DISPLAY.blit(P2_PT, (74 + (SIZE_X * POINT_SIZE) / 2, SIZE_Y * POINT_SIZE + 2))
 
-
 # Connecting
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setblocking(1)
 SERVER = (SERVER_ADDRESS, SERVER_PORT)
 sock.connect(SERVER)
 
+# SSL context creation
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.verify_mode = ssl.CERT_REQUIRED
 context.load_verify_locations('server.pem')
@@ -56,29 +57,21 @@ if not cert or ('commonName', 'SNAKE') not in cert['subject'][5]:
     raise Exception("ERROR")
 
 sock = secure_sock
-# Draw initial points
-# drawPoints(0,0)
 
 # Drawing food
 def drawFood(x, y, color):
     pygame.draw.circle(DISPLAY, color, (x * POINT_SIZE + POINT_SIZE / 2, y * POINT_SIZE + POINT_SIZE / 2),
                        POINT_SIZE / 2)
 
-
 # Drawing snake
 def generateSnake(snakeData, color):
     for point in snakeData:
         pygame.draw.rect(DISPLAY, color, (point[0] * POINT_SIZE, point[1] * POINT_SIZE, POINT_SIZE, POINT_SIZE))
 
-
-# Initial message to the server
-# sock.sendto("CLIENT".encode(),SERVER)
-
 # Additional variables
 lastData = None
 
-
-# Message composer
+# Simple message composer
 def composeMessage(message_type=None, nickname=None, user_id=None, \
                    game_name=None, game_id=None, d=None):
     header = Header(sender=1, message_type=message_type)
@@ -96,12 +89,9 @@ def composeMessage(message_type=None, nickname=None, user_id=None, \
     message = Message(header=header, body=body)
     return message.to_bytes()
 
-
 # Sending messages
 def sendMessage(message):
-    # print("send", message)
     sock.write(message)
-
 
 # Properly getting messages
 def getMessage(returnNone=False):
@@ -110,9 +100,7 @@ def getMessage(returnNone=False):
         data = b''
         while not data:
             data = sock.read(1024)
-        # print("data", data)
         unpacked = Message.from_bytes(data)
-        # print(unpacked.body.data)
     except socket.timeout as e:
         print("error")
         if returnNone:
@@ -121,23 +109,20 @@ def getMessage(returnNone=False):
             pass
     return unpacked
 
-
-# Checks if message is 200 OK
+# Checks if message indicates successful operation
 def messageOK(message):
-    # print(message.body.data)
     # return message.body.data["operation_success"]
     return True
-
 
 # Checks if message has expected type
 def messageType(message, message_type):
     return message.header.message_type == message_type
 
-
+# ID variables
 USER_ID = 0
 GAME_ID = 0
 
-# Temporary console part - managing nicks, games and joins
+# Console part - managing nicks, games and joins
 
 # Nickname input while
 while True:
@@ -156,7 +141,7 @@ while True:
         print("Uzyskano inny typ odpowiedzi niż oczekiwano. Koniec programu.")
         exit(1)
 
-# Gets current game list
+# Listing current games loop
 while True:
     sendMessage(composeMessage(MessageType.LIST_GAMES_CLIENT, user_id=USER_ID))
     inp = getMessage()
@@ -208,47 +193,48 @@ while True:
     elif (ch == "Q"):
         exit(0)
 
-# Initialization
+# PyGame initialization
 pygame.init()
 
-# Frames Per Second configuration
+# Frames Per Second configuration (PyGame)
 FPS = 30
 FramePerSec = pygame.time.Clock()
 
-# Game size (defines window size)
+# Game size (defines window size; PyGame)
 SIZE_X = 25
 SIZE_Y = 25
 POINT_SIZE = 24
 
-# Colors
+# Colors (PyGame)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 P1_C = (127, 255, 127)
 P2_C = (127, 127, 255)
 FOOD_COLOR = (255, 127, 0)
 
-# Creating window
+# Creating window (PyGame)
 DISPLAY = pygame.display.set_mode((SIZE_X * POINT_SIZE, SIZE_Y * POINT_SIZE + 16))
 DISPLAY.fill(WHITE)
 pygame.display.set_caption("Snake Client")
 
-# Fonts configuration
+# Fonts configuration (PyGame)
 font = pygame.font.SysFont("Courier", 12)
 
-# Adding HUD
+# Adding HUD (PyGame)
 pygame.draw.rect(DISPLAY, (0, 0, 0), (0, SIZE_Y * POINT_SIZE, SIZE_X * POINT_SIZE, SIZE_Y * POINT_SIZE + 16))
 PLAYER1 = font.render("Player 1: ", True, P1_C)
 DISPLAY.blit(PLAYER1, (2, SIZE_Y * POINT_SIZE + 2))
 PLAYER2 = font.render("Player 2: ", True, P2_C)
 DISPLAY.blit(PLAYER2, (2 + (SIZE_X * POINT_SIZE) / 2, SIZE_Y * POINT_SIZE + 2))
 
-# Main game loop
-last_key = "r"
+# Main game loop (PyGame)
+last_key = "r"  # default selected snake's direction
 last_update = time.time()
 
+# Additional variable for checking game exit
 endGame = False
 
-
+# Function for checking last pressed key
 def check_last_pressed_key(last_key):
     global endGame
     for event in pygame.event.get():
@@ -265,9 +251,10 @@ def check_last_pressed_key(last_key):
                 endGame = True
     return last_key
 
-
+# Additional variable
 last_last_key = ""
 
+# Main PyGame loop
 while True:
     now = time.time()
     last_key = check_last_pressed_key(last_key)
@@ -276,11 +263,8 @@ while True:
             pygame.quit()
             sock.close()
             sys.exit()
-    #if last_key != last_last_key:
     out = composeMessage(MessageType.SEND_MOVE, user_id=USER_ID, game_id=GAME_ID, d=last_key.encode())
-    # print("out", out)
     sendMessage(out)
-        #last_last_key = last_key
     if endGame:
         out = composeMessage(MessageType.EXIT_GAME_CLIENT, user_id=USER_ID, game_id=GAME_ID)
         sendMessage(out)
@@ -295,17 +279,13 @@ while True:
             exit(0)
     pygame.display.update()
     FramePerSec.tick(FPS)
-    # print("GAME")
     last_key = check_last_pressed_key(last_key)
     inp = getMessage(True)
     try:
-        # print(data.decode('utf-8'))
-        # input = eval(data.decode('utf-8'))["game_state"]
         input = inp.body.data
     except:
         input = lastData
     last_key = check_last_pressed_key(last_key)
-    # print(input)
     if True:
         if input != None:
             if lastData != None:
